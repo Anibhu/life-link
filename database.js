a0abef6bc6c4a275ff8b191512ec0bf4642a194d
arya3455 <banerjeearya2004@gmail.com>
2025-02-12 23:47:20 +0530

    Added registration page and styling

--- database.js
+++ database.js
@@ -6,11 +6,14 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-    host : process.env.DB_HOST,
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false,  // ✅ Fix: Allows AWS RDS connection without a certificate
+    },
 });
 
 export default pool;
